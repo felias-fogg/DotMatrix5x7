f7c46199e6b7867d0e76ce6ad8f7826b12e979e6
Bernhard Nebel <nebel@hinterm-ziel.de>
2022-01-11 19:08:08 +0100

    V 1.4.4 (idelFunc corrected)

--- examples/idleFunction/idleFunction.ino
+++ examples/idleFunction/idleFunction.ino
@@ -10,8 +10,19 @@ char str[]="Hello World!";
 void myIdle(long unsigned int period)
 {
   unsigned long time_now = millis();
-  while (millis() < time_now + period)
-    LowPower.idle(SLEEP_FOREVER); // go into idel mode until next timer interrupt
+  while (millis() < time_now + period) {
+    // go into idle mode until next timer interrupt
+#if defined(__AVR_ATmega328P__) || defined (__AVR_ATmega168__) || defined (__AVR_ATmega168P__) || defined (__AVR_ATmega88__)
+    LowPower.idle(SLEEP_FOREVER, ADC_ON, TIMER2_ON, TIMER1_ON, TIMER0_ON, SPI_ON, USART0_ON, TWI_ON);
+#elif defined __AVR_ATmega644P__ || defined (__AVR_ATmega1284P__)
+    LowPower.idle(SLEEP_FOREVER, ADC_ON, TIMER2_ON, TIMER1_ON, TIMER0_ON, SPI_ON, USART1_ON, USART0_ON, TWI_ON);
+#elif defined(__AVR_ATmega2560__)
+    LowPower.idle(SLEEP_FOREVER, ADC_ON, TIMER5_ON, TIMER4_ON, TIMER3_ON, TIMER2_ON, TIMER1_ON, TIMER0_ON, SPI_ON, USART3_ON, USART2_ON, USART1_ON, USART0_ON, TWI_ON);
+#elif defined(__AVR_ATmega32U4__)
+    LowPower.idle(SLEEP_FOREVER, ADC_ON, TIMER4_ON, TIMER3_ON, TIMER2_ON, TIMER1_ON, TIMER0_ON, SPI_ON, USART1_ON, TWI_ON, USB_ON);
+#else
+    #error "MCU not supported"
+#endif
 }
 
 void setup(void)
